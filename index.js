9ad5cec45cb785fb12eeb3dca68ecfdb580074d3
aleksandar.m.nikolov@gmail.com <aleksandar.m.nikolov@gmail.com>
2018-02-21 22:25:21 +0200

    exchanges added

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 
 module.exports = {
 
-  'Allcoin': require('./lib/allcoin')
+    'Abucoins': require('./lib/abucoins'),
+    'AEX': require('./lib/aex'),
+    'Allcoin': require('./lib/allcoin'),
+    'Bcex': require('./lib/bcex'),
+    'Bibox': require('./lib/bibox'),
+    'BigOne': require('./lib/bigone'),
+    'Bitbank': require('./lib/bitbank'),
+    'Bitbay': require('./lib/bitbay'),
+    'Bitflip': require('./lib/bitflip'),
+    'bitFlyer': require('./lib/bitflyer'),
+    'Bitgrail': require('./lib/bitgrail'),
+    'Bitlish': require('./lib/bitlish'),
+    'Bitsane': require('./lib/bitsane'),
+    'BitsBlockchain': require('./lib/bitsblockchain'),
+    'Bitso': require('./lib/bitso'),
+    'Bitstamp': require('./lib/bitstamp'),
+    'Bitz': require('./lib/bitz'),
+    'Bleutrade': require('./lib/bleutrade'),
+    'BTCAlpha': require('./lib/btcalpha'),
+    'BTCC': require('./lib/btcc'),
+    'BTCMarkets': require('./lib/btcmarkets'),
+    'BTCTrade': require('./lib/btctrade'),
+    'C-cex': require('./lib/ccex'),
+    'Cexio': require('./lib/cexio'),
+    'Cobinhood': require('./lib/cobinhood'),
+    'Coinbene': require('./lib/coinbene'),
+    'Coinfalcon': require('./lib/coinfalcon'),
+
 
 };
