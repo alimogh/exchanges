3e034e17d2667cf0a743131c67abd37011fc0412
aleksandar.m.nikolov@gmail.com <aleksandar.m.nikolov@gmail.com>
2018-02-24 16:09:05 +0200

    quadrigacx, zaif exchanges added

--- test.js
+++ test.js
@@ -2,9 +2,12 @@ const debug = require('debug')('cointrage:exchanges:test');
 const exchanges = require('./index');
 const exchange = exchanges.Quoine;
 
-exchange.getMarkets()
-    .then((m) => {
-        // exchange.getOrderBook('BTC', 'ETH').then((b) => {
-        //     debug(b);
-        // });
-    });
+async function init() {
+    let markets = await exchange.getMarkets();
+    debug(markets);
+
+    let orderBook = await exchange.getOrderBook('BTC', 'ETH');
+    debug(orderBook);
+}
+
+init();
