fb7b0f7e5098051f2fb0d14c637ca7816ddfe63a
Alexey Melnikov <alexey.ernest@gmail.com>
2018-03-03 11:49:04 +0300

    fix

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 const debug = require('debug')('cointrage:exchanges:test');
 const exchanges = require('./index');
-const exchange = exchanges.Quoine;
+const exchange = exchanges.YoBit;
 
 async function init() {
-    // let markets = await exchange.getMarkets();
-    // console.log(markets);
+    let markets = await exchange.getMarkets();
+    console.log(markets);
 
-    let orderBook = await exchange.getOrderBook('USD', 'DASH');
-    console.log(orderBook);
+    // let orderBook = await exchange.getOrderBook('USD', 'DASH');
+    // console.log(orderBook);
 }
 
 init();
