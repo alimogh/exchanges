fda3732ad26d64bfe5965e6b8cada930dca9c4f7
Alexey Melnikov <alexey.ernest@gmail.com>
2018-03-11 01:00:23 +0300

    Rightbtc fix

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const debug = require('debug')('cointrage:exchanges:test');
 const exchanges = require('./index');
-const exchange = exchanges.TDAX;
+const exchange = exchanges.RightBTC;
 
 async function init() {
     let markets = await exchange.getMarkets();
     console.log(markets);
 
-    let orderBook = await exchange.getOrderBook('ETH', 'KNC');
+    let orderBook = await exchange.getOrderBook('BTC', 'ETH');
     console.log(orderBook);
 }
 
