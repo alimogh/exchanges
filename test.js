8642d6c2d4c1c12ddf72b68dcc191d9c4dc69a18
Alexey Melnikov <alexey.ernest@gmail.com>
2018-03-11 00:31:42 +0300

    Bitbay fix

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const debug = require('debug')('cointrage:exchanges:test');
 const exchanges = require('./index');
-const exchange = exchanges.TDAX;
+const exchange = exchanges.Bitbay;
 
 async function init() {
     let markets = await exchange.getMarkets();
     console.log(markets);
 
-    let orderBook = await exchange.getOrderBook('ETH', 'LTC');
+    let orderBook = await exchange.getOrderBook('BTC', 'LTC');
     console.log(orderBook);
 }
 
