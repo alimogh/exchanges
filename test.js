9bd13ef2f7e1b30f017551c38ffd1e7fd95e8929
Alexey Melnikov <alexey.ernest@gmail.com>
2018-03-11 00:54:48 +0300

    TDax fix

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const debug = require('debug')('cointrage:exchanges:test');
 const exchanges = require('./index');
-const exchange = exchanges.Bitbay;
+const exchange = exchanges.TDAX;
 
 async function init() {
     let markets = await exchange.getMarkets();
     console.log(markets);
 
-    let orderBook = await exchange.getOrderBook('BTC', 'LTC');
+    let orderBook = await exchange.getOrderBook('ETH', 'KNC');
     console.log(orderBook);
 }
 
