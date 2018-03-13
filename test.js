5ad338c2588cd03abd4f339768e2e5352a5f533c
Alexey Melnikov <alexey.ernest@gmail.com>
2018-03-13 17:35:52 +0300

    Right btc error message url

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const debug = require('debug')('cointrage:exchanges:test');
 const exchanges = require('./index');
-const exchange = exchanges.RightBTC;
+const exchange = exchanges.Qryptos;
 
 async function init() {
     let markets = await exchange.getMarkets();
     console.log(markets);
 
-    let orderBook = await exchange.getOrderBook('BTC', 'ETH');
+    let orderBook = await exchange.getOrderBook('ETH', 'AMLT');
     console.log(orderBook);
 }
 
