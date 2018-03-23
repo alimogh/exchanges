d0098bf6639f9a6a0fd069a45ee8bee5ee555e6b
aleksandar.m.nikolov@gmail.com <aleksandar.m.nikolov@gmail.com>
2018-03-23 20:51:19 +0200

    Binance methods

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
 module.exports = {
-    MARKETS: ['ETH', 'BTC', 'XBT', 'USDT', 'USD']
+    MARKETS: ['ETH', 'BTC', 'XBT', 'USDT', 'USD'],
+    Binance: {
+        api_key: '',
+        api_secret: '',
+        recv_window: 5000
+    }
 };
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const debug = require('debug')('cointrage:exchanges:test');
 const exchanges = require('./index');
-const exchange = exchanges.Bitfinex;
+const exchange = exchanges.Binance;
 
 const STANDARD_MAPPINGS = {
     'DAT': 'DATA'
@@ -10,8 +10,28 @@ async function init() {
     // let markets = await exchange.getMarkets();
     // debug(markets);
 
-    let orderBook = await exchange.getOrderBook('BTC', 'DATA');
-    debug(orderBook);
+    // let orderBook = await exchange.getOrderBook('BTC', 'DATA');
+    // debug(orderBook);
+
+    // let accountInfo = await exchange.getAccountInfo({});
+    // debug(accountInfo);
+
+    // const data = {
+    //     symbol: 'ETHBTC',
+    //     side: 'BUY',
+    //     type: 'MARKET',
+    //     quantity: 100
+    // };
+    // let orderInfo = await exchange.createOrder(data);
+    // debug(orderInfo);
+
+    // const data = {
+    //     symbol: 'SUBBTC'
+    // };
+    // let ordersData = await exchange.getAllOrders(data);
+    // debug(ordersData);
+
+
 }
 
 init();
